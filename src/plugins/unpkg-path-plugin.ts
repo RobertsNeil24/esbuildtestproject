f4bc8e16686c05cbd092fc29ff49f57bffe765ce
Neil Roberts <nsr2412@gmail.com>
2021-08-04 09:19:35 +0200

    refactored resolve to use filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -15,23 +15,24 @@ const fileCache = localForage.createInstance({
 //   console.log(color)
 // })()
  
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
+
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a'}
+      });
+
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href
+        }
+      })
+
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if(args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
-
-        if (args.path.includes('./' || args.path.includes('../'))) {
-          return {
-            namespace: 'a',
-            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href
-          }
-        }
-
+      
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`
@@ -45,10 +46,8 @@ export const unpkgPathPlugin = () => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              const message = require('axios');
-              console.log(message);
-            `,
+            contents: inputCode
+            ,
           };
         } 
 

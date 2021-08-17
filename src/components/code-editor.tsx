5a12f8cc47b325d7fce218d5cebbdf95dc5f326d
Neil Roberts <nsr2412@gmail.com>
2021-08-17 15:29:13 +0200

    updated syntax highlighting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,8 +1,11 @@
-import './code-editor.css';
-import { useRef } from 'react';
+import "./code-editor.css";
+import './syntax.css';
+import { useRef } from "react";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
+import codeShift from "jscodeshift";
+import HighLighter from "monaco-jsx-highlighter";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -10,35 +13,55 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
-    const editorRef = useRef<any>(); 
-  
-    const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+  const editorRef = useRef<any>();
+
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
     editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
 
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+
+    const highlighter = new HighLighter(
+        // @ts-ignore
+        window.monaco,
+        codeShift,
+        monacoEditor
+    );
+    highlighter.highLightOnDidChangeModelContent(
+        () => {},
+        () => {},
+        undefined,
+        () => {}
+    );
   };
 
   const onFormatClick = () => {
     // get current value from editor
     const unformatted = editorRef.current.getModel().getValue();
     // format the value
-    const formatted = prettier.format(unformatted, {
-        parser: 'babel',
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
         plugins: [parser],
         useTabs: false,
         semi: true,
-        singleQuote: true
-    }).replace(/\n$/, '');
+        singleQuote: true,
+      })
+      .replace(/\n$/, "");
     // set the formatted value back in the editor
     editorRef.current.setValue(formatted);
   };
 
   return (
     <div className="editor-wrapper">
-      <button className="button button-format is-primary is-small" onClick={onFormatClick}>Format</button>
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}

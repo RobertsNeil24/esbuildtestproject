26f564658a67a130e28e01941b8a40c394393ac0
Neil Roberts <nsr2412@gmail.com>
2021-08-19 11:59:32 +0200

    improved resizable and added debounce logic

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
 import bundle from "../bundler";
@@ -8,18 +8,27 @@ const CodeCell = () => {
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-//   const onClick = async () => {
-//     const output = await bundle(input);
-//     setCode(output);
-//   };
+  useEffect(() => {
+   const timer = setTimeout( async () => {
+        const output = await bundle(input);
+        setCode(output);
+    }, 1000);
+
+    return () => {
+        clearTimeout(timer);
+    }
+  }, [input]);
+
 
   return (
     <Resizable direction="vertical">
     <div style={{ height: '100%', display:'flex', flexDirection: 'row' }}>
+      <Resizable direction="horizontal">
       <CodeEditor
         initialValue="const a = 1;"
         onChange={(value) => setInput(value)}
       />
+      </Resizable>
       <Preview code={code} />
     </div>
     </Resizable>
